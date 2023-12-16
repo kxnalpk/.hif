a0fda635f7853d52f0caeccf63286dfccd120a65
Hiftie <hiftielabs@gmail.com>
2023-12-16 21:48:14 +0530

    Decreased the file size by 70-80%

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{
     env,
     fs::{self, OpenOptions},
-    io::Write,
+    io::{Write, Read},
     path::PathBuf,
 };
 
@@ -11,6 +11,7 @@ use colors_transform::Rgb;
 use image::{GenericImageView};
 use skia_safe::{AlphaType, Color4f, ColorType, EncodedImageFormat, ImageInfo, Paint, Rect, Surface};
 use css_color_parser::Color as CssColor;
+use flate2::{write::GzEncoder, Compression};
 
 static TEMP_RESULT_PATH: &str = "temp.png";
 
@@ -25,106 +26,90 @@ impl Sam {
 
     fn convert_png_to_hif(path: PathBuf) -> Result<(), std::io::Error> {
         let img = image::open(&path).expect("File not found!");
-        let mut str = String::new();
-        let mut last_line = 0;
-
+    
+        let mut pixel_data: Vec<u8> = Vec::new();
+    
+        let width_bytes: [u8; 4] = img.width().to_ne_bytes();
+        let height_bytes: [u8; 4] = img.height().to_ne_bytes();
+    
+        pixel_data.extend_from_slice(&width_bytes);
+        pixel_data.extend_from_slice(&height_bytes);
+    
         for pixel in img.pixels() {
-            let hex_color = Rgb::from(
-                pixel.2 .0[0] as f32,
-                pixel.2 .0[1] as f32,
-                pixel.2 .0[2] as f32,
-            )
-            .to_css_hex_string();
-
-            if last_line != pixel.1 {
-                str.push_str("\n");
-                last_line = pixel.1;
-            }
-            str.push_str(&hex_color.replace("#", ""));
+            pixel_data.push(pixel.2[0]); // R
+            pixel_data.push(pixel.2[1]); // G
+            pixel_data.push(pixel.2[2]); // B
         }
-
-        if let Some(path_str) = &path.to_str() {
-            let height: u32 = img.height();
-            let width: u32 = img.width();
-
-            let height_bytes: [u8; 4] = height.to_ne_bytes();
-            let width_bytes: [u8; 4] = width.to_ne_bytes();
-            let path_to_hif = path_str.replace(".png", ".hif");
-
-            let mut file = OpenOptions::new()
-                .write(true)
-                .create(true)
-                .open(path_to_hif)
-                .expect("Couldn't write");
-            let string_bytes: Vec<u8> = Vec::from(str.as_bytes());
-
-            file.write_all(&width_bytes)?;
-            file.write_all(&height_bytes)?;
-            file.write_all(&string_bytes)?;
-            file.flush()?;
-        } else {
-            println!("{}", "Couldn't find")
-        }
-
+    
+        let path_to_hif = path.with_extension("hif");
+    
+        fs::write(path_to_hif, &pixel_data)?;
+    
         Ok(())
-    }
+    }    
 
     fn hif_to_png(path: PathBuf) -> (u32, u32) {
-        let mut contents: Vec<u8> = fs::read(&path).expect("Couldn't read file.");
-        let binding: Vec<_> = contents.drain(0..8).collect();
-
-        let width = Sam::convert_bytes_to_u32_ne(&binding[0..4]);
-        let height = Sam::convert_bytes_to_u32_ne(&binding[4..8]);
-
-        let sanitized_content = String::from_utf8_lossy(&contents).replace("\n", "");
-
-        let result: Vec<&str> = sanitized_content
-            .as_bytes()
-            .chunks(6)
-            .map(std::str::from_utf8)
-            .collect::<Result<_, _>>()
-            .expect("Invalid UTF-8 sequence in the input string");
-
+        let contents = fs::read(&path).expect("Couldn't read file.");
+    
+        let (width, height) = (
+            Sam::convert_bytes_to_u32_ne(&contents[0..4]),
+            Sam::convert_bytes_to_u32_ne(&contents[4..8]),
+        );
+    
+        let pixel_data = &contents[8..];
+    
         let info = ImageInfo::new(
             (width as i32, height as i32),
             ColorType::RGBA8888,
-            AlphaType::Opaque,
+            AlphaType::Unpremul,
             None,
         );
-
+    
         let mut surface = Surface::new_raster(&info, None, None).unwrap();
         let canvas = surface.canvas();
-
-        for (i, color) in result.iter().enumerate() {
-            let hex = "#".to_owned() + color;
-
-            let parsed_color = hex
-                .parse::<CssColor>()
-                .expect("Failed to convert Hex to RGB");
-            let color4f = Color4f::new(
-                parsed_color.r as f32,
-                parsed_color.g as f32,
-                parsed_color.b as f32,
-                0.004 as f32,
-            );
+    
+        for (i, chunk) in pixel_data.chunks_exact(3).enumerate() {
+            let r = chunk[0];
+            let g = chunk[1];
+            let b = chunk[2];
+            let color4f = Color4f::new(r as f32 / 255.0, g as f32 / 255.0, b as f32 / 255.0, 1.0);
             let paint = Paint::new(color4f, None);
-            if i == 0 {
-                println!("{:?}", paint)
-            }
-            let x = i % width as usize;
-            let y = i / width as usize;
-
+    
+            let x = (i as u32) % width;
+            let y = (i as u32) / width;
+    
             let rect = Rect::from_point_and_size((x as f32, y as f32), (1.0, 1.0));
             canvas.draw_rect(rect, &paint);
         }
-
+    
         let image = surface.image_snapshot();
+    
+        if let Some(data) = image.encode_to_data(EncodedImageFormat::PNG) {
+            let bytes = data.as_bytes();
+            
+            fs::write(TEMP_RESULT_PATH, bytes).expect("Failed to write image data to file");
+        }        
+    
+        (width, height)
+    }     
 
-        if let Some(data) = image.encode(None, EncodedImageFormat::PNG, 100) {
-            fs::write(TEMP_RESULT_PATH, &*data).expect("Failed to write image data to file");
+    fn compress_hif_file(input_path: &PathBuf) -> Result<(), std::io::Error> {
+        let input = fs::File::open(input_path)?;
+        let mut reader = std::io::BufReader::new(input);
+        let output_path = input_path.with_extension("hif.gz");
+        let output = fs::File::create(&output_path)?;
+        let mut writer = std::io::BufWriter::new(output);
+        let mut encoder = GzEncoder::new(writer, Compression::default());
+
+        let mut buffer = [0; 4096];
+        while let Ok(count) = reader.read(&mut buffer) {
+            if count == 0 {
+                break;
+            }
+            encoder.write_all(&buffer[..count])?;
         }
-
-        return (width, height);
+        encoder.finish()?;
+        Ok(())
     }
 
     fn run_app(file_path: PathBuf) -> Result<(), eframe::Error> {
@@ -152,7 +137,7 @@ impl Default for SamPreview {
     fn default() -> Self {
         let image_data = std::fs::read(TEMP_RESULT_PATH).expect("Failed to read image file");
 
-        fs::remove_file(TEMP_RESULT_PATH).expect("File delete failed on TEMP_RESULT_PATH");
+        fs::remove_file(TEMP_RESULT_PATH).expect("File delete failed on TEMP RESULT PATH");
 
         Self {
             image: RetainedImage::from_image_bytes(TEMP_RESULT_PATH, &image_data).unwrap(),
@@ -170,22 +155,38 @@ impl eframe::App for SamPreview {
 
 fn main() -> Result<(), eframe::Error> {
     let args: Vec<String> = env::args().collect();
-    let file_path: PathBuf = (&args[1]).into();
+
+    if args.len() < 2 {
+        eprintln!("Not enough arguments provided.");
+        return Ok(());
+    }
 
     if &args[1] == "compile" {
         if args.len() < 3 {
-            panic!("Secondary argument ('path') not provided. Example: `cargo run compile ~/image.png`")
+            eprintln!("Secondary argument ('path') not provided. Example: `cargo run compile ~/image.png`");
+            return Ok(());
         }
 
         let path: PathBuf = (&args[2]).into();
 
         match Sam::convert_png_to_hif(path) {
-            Ok(()) => println!("{}", "Successfully converted PNG to hif"),
-            Err(_) => println!("{}", "Failed to convert PNG to hif"),
+            Ok(()) => println!("Successfully converted PNG to HIF"),
+            Err(_) => println!("Failed to convert PNG to HIF"),
         }
-
-        Ok(())
+    } else if &args[1] == "compress" {
+        if args.len() < 3 {
+            eprintln!("Path argument not provided. Usage: `cargo run compress path/to/file.hif`");
+            return Ok(());
+        }
+        let path: PathBuf = (&args[2]).into();
+        match Sam::compress_hif_file(&path) {
+            Ok(()) => println!("Successfully compressed .hif file"),
+            Err(e) => println!("Failed to compress .hif file: {}", e),
+        }
     } else {
-        Sam::run_app(file_path)
+        let file_path: PathBuf = (&args[1]).into();
+        Sam::run_app(file_path)?;
     }
+
+    Ok(())
 }
